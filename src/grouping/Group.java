e4d60b74001c42e4e1e4d7e087ba8c0549300e16
MoisesKersch <lightspeedoff@gmail.com>
2019-01-01 01:06:42 -0200

    Update

--- src/grouping/Group.java
+++ src/grouping/Group.java
@@ -118,23 +118,39 @@ public class Group
 
 	public static void main(String[] args)
 	{
+		// 1 carregando os dados todos desagrupados
 		List<Foo> foos = getLocalFoo();
 
-		List<Eua> euas = new ArrayList<>(); // criado endereco 1
+		// 2 criando o array principal que vai guardar os dados agrupados
+		// * Esse array ira manter sempre apontado, durante toda a execução do codigo,
+		// par um lugar estatico na memoria. Vamos chamar esse endereço de m1
+		List<Eua> euas = new ArrayList<>(); 
 
-		Ua ua = new Ua();
+		// * 3Varievel auxiliar para guardar dados que vao entrar no sub array do array principal
+		// * Essa variavel tambem utilizara muito a tecnica de apontar para um lugar especifico na memoria.
+		Ua ua = null;
 
+		// 4 loop dentro do array desagrupado
 		for (Foo foo : foos)
 		{
-			Eua eua = gEua(euas, foo, ua); // ua apontado uara o endereço 2
-
-			if (eua.getUas().size() > 0)
+			// 5 essa varivel vai apontar para um espaço na memoria inicializado no metodo gEua.
+			// vamos chama-lo de m2
+			// no metodo gEua estamos passando 2 endereços de memoria ja citados
+			// gEua(m1, ?, m2) 
+			// 14 estamos agora apontando e eua abaixo na m4 gerada pela funcao gEua
+			Eua eua = gEua(euas, foo, ua); 
+			
+			// caso o ua nao esteja apontado para o mesmo lugar do euaGetUas
+			// entao o apontamos para essa direção
+			if ( !(ua == eua.getUas().get(eua.getUas().size() - 1)) )
+			{
 				ua = eua.getUas().get(eua.getUas().size() - 1);
+			}
 
+			// criamos um item, a adicionamos dentro desse eu.
 			A a = new A();
 			a.setNome(foo.getNome());
 			ua.getAs().add(a);
-
 		}
 
 		System.out.println(euas);
@@ -142,6 +158,8 @@ public class Group
 
 	public static Eua gEua(List<Eua> euas, Foo foo, Ua ua)
 	{
+		// 6 vamos entrar dentro do endereço de memoria m1 nessa execuçao
+		// na primera interaçao como o array eh vazio ele nao vai entrar nesse loop
 		for (Eua eua : euas)
 		{
 			if (eua.getEmpresaId().equals(foo.getEmpresaId()))
@@ -156,15 +174,27 @@ public class Group
 				return eua;
 			}
 		}
-
+		
+		// 7 Criando uma nova instancia do objeto principal 
+		// vamos chamar esse endereço de m4 
 		Eua eua = new Eua(); // criado endereço 2
 
+		// 8 adiciona o nome da empresa no objeto m4
 		eua.setEmpresaId(foo.getEmpresaId());
+		
+		// 9 aponta o ua para algum lugar na memoria, vamos chamar esse lugar de m5
 		ua = new Ua();
 		
+		// 10 adiciona o m5 dentro do m4
 		eua.getUas().add(ua);
+		
+		// 11 adiciona nome do usuario na m5
 		ua.setUsuarioId(foo.getUsuarioId());
+		
+		// 12 vadiciona o objeto na m1
 		euas.add(eua);
+		
+		// 13 retorna a m4
 		return eua;
 	}
 }
@@ -236,6 +266,7 @@ class A
 	}
 }
 
+// objeto principal
 class Foo
 {
 	private Long id;
